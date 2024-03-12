874f79ff02bdcc9dbcb879f1e1c569b9f59c6396
anthroponymy <ksnavinkumar.diary@gmail.com>
2024-03-12 23:23:41 +0530

    added some code

--- apps/core/src/app.ts
+++ apps/core/src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello, world!');
+// write a sample code to test the logger usinf express and winston logger
+import express from 'express';
+import logger from './logger';
+
+const app = express();
+app.use(logger);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+export default app;
+
