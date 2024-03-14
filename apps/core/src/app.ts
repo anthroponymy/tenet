800481e95fe7d3b537c49447aafb4a4454faf2d4
NavinKumar <123103678+anthroponymy@users.noreply.github.com>
2024-03-14 13:14:59 +0000

    added more code

--- apps/core/src/app.ts
+++ apps/core/src/app.ts
@@ -1,17 +1,26 @@
-// write a sample code to test the logger usinf express and winston logger
-import express from 'express';
-import logger from './logger';
+import * as fs from "fs";
+import { AuditLog } from "./auditlog.js";
+import type { GraphResponse, Logs } from "./model.js";
 
-const app = express();
-app.use(logger);
+const filePath = "./AuditCommitByFilter.json";
+function printLogs(logs: Logs[]): void {
+	console.log(JSON.stringify(logs, null, 2));
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+
+fs.readFile(filePath, "utf8", (err, data) => {
+	if (err) {
+		console.error("Error reading file:", err);
+		return;
+	}
+
+	try {
+		const jsonData: GraphResponse = JSON.parse(data);
+		const logs: Logs[] = jsonData.data.auditCommitByFilter.nodes.map((node) =>
+			AuditLog.processLogNode(node)
+		);
+		printLogs(logs);
+	} catch (err) {
+		console.error("Error parsing JSON:", err);
+	}
 });
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-
-export default app;
-
